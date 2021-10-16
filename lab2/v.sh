e744d2b73077d5a93eeeb13ad7637e0882e5bd84
maypok86 <alex.mayshev.86@gmail.com>
2021-10-16 12:22:45 +0300

    add lab2/v.sh

--- lab2/v.sh
+++ lab2/v.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 bash iv.sh
+echo -n > v.txt
+
+ppid=0
+sum=0
+count=0
+while read line
+do
+    temp_ppid=$(echo $line | awk '{ print $5 }')
+    if [[ $ppid -eq $temp_ppid ]]; then
+	average=$(echo $line | awk '{ print $8 }')
+	sum=$(echo "$sum+$average" | bc -l)
+        count=$(echo "$count+1" | bc -l)
+    else
+	echo 'Average_Sleeping_Children_of_ParentID=' $ppid ' is ' $(echo "$sum/$count" | bc -l) >> v.txt
+	ppid=$(echo $line | awk '{ print $5 }')
+	sum=$(echo $line | awk '{ print $8 }')
+	count=1
+    fi
+    echo $line >> v.txt
+done < iv.txt
+echo "Average_Sleeping_Children_of_ParentID=" $ppid " is " $(echo "$sum/$count" | bc -l) >> v.txt
